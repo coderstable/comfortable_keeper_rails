bf449947c847f244f100e21176a0bc9941ffd186
ycoda <tankaku2003@yahoo.co.jp>
2016-11-29 22:25:52 +0900

    review fix#1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
+ActiveRecord::Schema.define(version: 20161119083508) do
+  
   create_table "devices", force: :cascade do |t|
     t.string   "mac_address"
     t.string   "preset_temperatuire"
