58a3086155638b192eb430cfb888ef88beff9dac
ycoda <tankaku2003@yahoo.co.jp>
2016-12-06 10:19:24 +0900

    ルーティング、get   'device'   => 'top#device'をコメントアウト

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,4 +1,6 @@
 class Device < ApplicationRecord
   belongs_to :user
-  validates :preset_temperature, numericality: { only_integer: true, greater_than: 10, less_than: 40 }
+  validates :preset_temperature, presence: true,
+            numericality: { only_integer: true, greater_than: 20, less_than: 30 }
+  validates :device_name, presence: true
 end
