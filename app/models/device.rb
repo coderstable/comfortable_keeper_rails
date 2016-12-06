828b51c2d138ec9cf518b02dff9408d0897f0025
ycoda <tankaku2003@yahoo.co.jp>
2016-12-06 16:55:09 +0900

    model device validation 作業途中、一旦コミット

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,8 @@
 class Device < ApplicationRecord
   belongs_to :user
   validates :preset_temperature, presence: true,
-            numericality: { only_integer: true, greater_than: 20, less_than: 30 }
+            numericality: { only_integer: true, greater_than: 20, less_than: 30, message: "設定温度は20〜30の範囲で入力して下さい" }
   validates :device_name, presence: true
+  validates :preset_temperature, presence: { message: "温度設定を入力して下さい"}
+  validates :device_name, presence: { message: "デバイス名を入力して下さい"}
 end
