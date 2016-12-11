4a0b047428462a7248f12ad574d1d1b2366383cb
ycoda <tankaku2003@yahoo.co.jp>
2016-12-11 10:46:11 +0900

    デバイス登録時、バリデーションメッセージの修正、その他 #15

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,8 +1,7 @@
 class Device < ApplicationRecord
   belongs_to :user
-  validates :preset_temperature, presence: true,
-            numericality: { only_integer: true, greater_than: 20, less_than: 30, message: "設定温度は20〜30の範囲で入力して下さい" }
-  validates :device_name, presence: true
-  validates :preset_temperature, presence: { message: "温度設定を入力して下さい"}
-  validates :device_name, presence: { message: "デバイス名を入力して下さい"}
+  validates :device_name, presence:  { message: "デバイス名が入力されていません"}
+  validates :preset_temperature, presence: { message: "設定温度が入力されていません。"},
+            numericality: { only_integer: true, greater_than_or_equal_to: 20, less_than_or_equal_to: 30, message: "設定温度は20〜30の範囲で入力して下さい" }
+  validates :device_communication, inclusion: { in: [true, false] }
 end
