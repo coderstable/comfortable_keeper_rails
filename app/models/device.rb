dd15a5b13000e22cd5e9f0be96b18ad8db04a114
ycoda <tankaku2003@yahoo.co.jp>
2016-12-18 14:22:35 +0900

    モデルdevice.rbにdevice_nameの文字数制限を追加

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,5 @@
 class Device < ApplicationRecord
   belongs_to :user
-  validates :device_name, presence:  { message: "デバイス名が入力されていません"}
-  validates :preset_temperature, presence: { message: "設定温度が入力されていません。"},
-            numericality: { only_integer: true, greater_than_or_equal_to: 20, less_than_or_equal_to: 30, message: "設定温度は20〜30の範囲で入力して下さい" }
-  validates :device_communication, inclusion: { in: [true, false] }
+  validates :device_name, length: { in: 2..30 }
+  validates :preset_temperature, numericality: { only_integer: true, greater_than: 10, less_than: 40 }
 end
